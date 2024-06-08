e6632e45ee7883167cb8e878a2acb493f3b1807d
Yuuya <83148287+YuuyaGitHub@users.noreply.github.com>
2024-06-08 19:25:33 +0900

    Add files via upload

---
+++ Font Test Program/Form1.Designer.cs
@@ -0,0 +1,113 @@
+﻿namespace Font_Test_Program
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必要なデザイナー変数です。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 使用中のリソースをすべてクリーンアップします。
+        /// </summary>
+        /// <param name="disposing">マネージド リソースを破棄する場合は true を指定し、その他の場合は false を指定します。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows フォーム デザイナーで生成されたコード
+
+        /// <summary>
+        /// デザイナー サポートに必要なメソッドです。このメソッドの内容を
+        /// コード エディターで変更しないでください。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.statusBar1 = new System.Windows.Forms.StatusBar();
+            this.mainMenu1 = new System.Windows.Forms.MainMenu(this.components);
+            this.menuItem1 = new System.Windows.Forms.MenuItem();
+            this.menuItem2 = new System.Windows.Forms.MenuItem();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.SuspendLayout();
+            // 
+            // statusBar1
+            // 
+            this.statusBar1.Location = new System.Drawing.Point(0, 307);
+            this.statusBar1.Name = "statusBar1";
+            this.statusBar1.Size = new System.Drawing.Size(759, 22);
+            this.statusBar1.TabIndex = 0;
+            // 
+            // mainMenu1
+            // 
+            this.mainMenu1.MenuItems.AddRange(new System.Windows.Forms.MenuItem[] {
+            this.menuItem1,
+            this.menuItem2});
+            // 
+            // menuItem1
+            // 
+            this.menuItem1.Index = 0;
+            this.menuItem1.Text = "&Change Font";
+            this.menuItem1.Click += new System.EventHandler(this.menuItem1_Click);
+            // 
+            // menuItem2
+            // 
+            this.menuItem2.Index = 1;
+            this.menuItem2.Text = "&Enter Sample Text";
+            this.menuItem2.Click += new System.EventHandler(this.menuItem2_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.textBox1.Location = new System.Drawing.Point(6, 2);
+            this.textBox1.MaxLength = 2147483640;
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.ScrollBars = System.Windows.Forms.ScrollBars.Both;
+            this.textBox1.Size = new System.Drawing.Size(747, 299);
+            this.textBox1.TabIndex = 1;
+            this.textBox1.WordWrap = false;
+            // 
+            // timer1
+            // 
+            this.timer1.Enabled = true;
+            this.timer1.Interval = 1;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(759, 329);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.statusBar1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Menu = this.mainMenu1;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Font Test Program";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.StatusBar statusBar1;
+        private System.Windows.Forms.MainMenu mainMenu1;
+        private System.Windows.Forms.MenuItem menuItem1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.MenuItem menuItem2;
+    }
+}
+
---
+++ Font Test Program/Form1.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.IO;
+using System.Windows.Forms;
+
+namespace Font_Test_Program
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void menuItem1_Click(object sender, EventArgs e)
+        {
+            FontDialog fd = new FontDialog();
+
+            fd.Font = textBox1.Font;
+            fd.MinSize = 1;
+            fd.MaxSize = 9999;
+            fd.ShowEffects = false;
+            fd.FontMustExist = true;
+            fd.AllowVerticalFonts = false;
+            fd.ShowEffects = false;
+
+            if (fd.ShowDialog() != DialogResult.Cancel)
+            {
+                textBox1.Font = fd.Font;
+            }
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            // Show Font Name in Status Bar
+            statusBar1.Text = "Font Name: " + textBox1.Font;
+        }
+
+        private void menuItem2_Click(object sender, EventArgs e)
+        {
+            EnterSampleText();
+        }
+
+        private void EnterSampleText()
+        {
+            // Get the application executable directory
+            string appDirectory = AppDomain.CurrentDomain.BaseDirectory;
+            // Construct the file path
+            string filePath = Path.Combine(appDirectory, "sampletext.txt");
+
+            if (File.Exists(filePath))
+            {
+                // If the file exists, read its contents and display them in textBox1
+                string fileContent = File.ReadAllText(filePath);
+                textBox1.Text = fileContent;
+            }
+            else
+            {
+                // If the file does not exist, prompt to perform the default sample text
+                ConfimDefaultSampleText();
+            }
+        }
+        private void ConfimDefaultSampleText()
+        {
+            DialogResult result = MessageBox.Show("sampletext.txt is missing. Would you like to load the application's built-in text?",
+                "Enter Sample Text",
+                MessageBoxButtons.YesNo,
+                MessageBoxIcon.Exclamation);
+
+            if (result == DialogResult.Yes)
+            {
+                // This is the built-in sample text embedded in the application
+
+                // Show Sample Text
+                string sampleText1 = "This is a Sample Text\r\nこれはサンプルテキストです";
+                textBox1.Text = sampleText1;
+            }
+        }
+    }
+}
